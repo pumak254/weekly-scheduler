f73dccff57487ef37803363aa01825679a7085f4
pumak254 <cesar_chavira@att.net>
2016-04-29 20:09:58 +0000

    sign up complete skip tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(username: 'gecko', password: 'AAA', password_confirmation: 'AAA')
